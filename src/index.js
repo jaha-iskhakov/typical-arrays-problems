f581f199c48a1ea387d9a9e5d6bd26f4c0322647
Jakhongir Iskhakov <jahaiskhakov@gmail.com>
2021-03-09 18:35:02 +0500

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+exports.min = function min(array) {
+  return array === undefined
+      ? 0
+      : array.reduce((min, el) => (el < min ? el : min), 0);
+};
 
-exports.min = function min (array) {
- let a = array[0] + array[2];
-  return a;
-}
+exports.max = function max(array) {
+  return array === undefined
+      ? 0
+      : array.reduce((max, el) => (el > max ? el : max), 0);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  return array === undefined || array.length === 0
+      ? 0
+      : array.reduce((sum, el) => sum + el) / array.length;
+};
